d23ceec28b7fef5bc28c67e1ef4d8ec455ebf8c0
unknown <dgloria.web@gmail.com>
2020-05-04 14:04:25 +0100

    'release'

--- index.js
+++ index.js
@@ -3,24 +3,57 @@ const path = require('path')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-if(req.url === '/'){
-    fs.readFile(path.join(__dirname, 'reference','movie.json'),(err, content) => {
-        if (err) throw err 
-        res.writeHead(200, {'Content-Type':'text/html'})
-        res.end(content)
-    })    
-}
-if(req.url === '/api/movies'){
-    fs.readFile(path.join(__dirname, 'reference','movie.json'),(err, content) => {
-        if (err) throw err 
-        res.writeHead(200, {'Content-Type':'application/json'})
-        res.end(content)
-    })    
-}
+  let filePath = path.join(
+    __dirname,
+    'public',
+    req.url === '/' ? 'index.html' : req.url
+  )
+
+  let extname = path.extname(filePath)
+  let contentType = 'text/html'
+
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript'
+      break
+    case '.css':
+      contentType = 'text/css'
+      break
+    case '.json':
+      contentType = 'application/json'
+      break
+    case '.png':
+      contentType = 'image/png'
+      break
+    case '.jpg':
+      contentType = 'image/jpg'
+      break
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == 'ENOENT') {
+        //page not found
+        fs.readFile(path.join(__dirname, 'public', '404.html'),
+          (err, content) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' })
+            res.end(content, 'utf8')
+          })
+      } else {
+        //other error
+        res.writeHead(500)
+        res.end(`Server Error: ${err.code}`)
+      }
+    } else {
+      //success
+      res.writeHead(200, { 'Content-Type': contentType })
+      res.end(content, 'utf8')
+    }
+  })
 })
 
 const PORT = process.env.PORT || 5000
-server.listen(PORT, () =>console.log(`Server running on port ${PORT}`))
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
 // const Bike = require('./bike')
 // const bike1 = new Bike('bike3', 'dfksh')
